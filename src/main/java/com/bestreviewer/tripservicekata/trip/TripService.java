4e038fc8ad08c8e9b49f36ab1fd7db116a7eb6dd
Hyejin-Lee-ANC <66674193+Hyejin-Lee-ANC@users.noreply.github.com>
2021-01-15 13:28:01 +0900

    getTripsByUser() 의 for() 를 user.isFriendWith() 로 대체

--- src/main/java/com/bestreviewer/tripservicekata/trip/TripService.java
+++ src/main/java/com/bestreviewer/tripservicekata/trip/TripService.java
@@ -12,14 +12,14 @@ public class TripService {
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedUser = getLoggedInUser();
-		boolean isFriend = false;
 		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
+			boolean isFriend = user.isFriendWith(loggedUser);
+//			for (User friend : user.getFriends()) {
+//				if (friend.equals(loggedUser)) {
+//					isFriend = true;
+//					break;
+//				}
+//			}
 			if (isFriend) {
 				tripList = tripsBy(user);
 			}
