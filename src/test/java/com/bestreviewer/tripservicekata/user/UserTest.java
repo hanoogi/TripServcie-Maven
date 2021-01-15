be0a2f270828b984bc2d6a5b4684035a5ef50658
Hyejin-Lee-ANC <66674193+Hyejin-Lee-ANC@users.noreply.github.com>
2021-01-15 13:22:14 +0900

    UserTest 추가 - 친구인 경우 테스트 추가 - 친구가 아닌 경우 테스트 추가 - 친구인지 확인을 위한 메소드를 user.java 에 추가

--- src/test/java/com/bestreviewer/tripservicekata/user/UserTest.java
+++ src/test/java/com/bestreviewer/tripservicekata/user/UserTest.java
@@ -1,5 +1,31 @@
 package com.bestreviewer.tripservicekata.user;
 
-public class UserTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static com.bestreviewer.tripservicekata.trip.UserBuilder.aUser;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserTest {
+    private static final User CHULSOO = new User();
+    private static final User SOONHEE= new User();
+
+    @Test
+    @DisplayName("친구가 아닌 경우")
+    public void testInformWhenUsersAreNotFrields(){
+        User user = aUser()
+                .friendsWith(CHULSOO)
+                .build();
+        assertFalse(user.isFriendWith(SOONHEE));
+    }
+
+    @Test
+    @DisplayName("친구인 경우")
+    public void testInformWhenUsersAreFrields(){
+        User user = aUser()
+                .friendsWith(CHULSOO,SOONHEE)
+                .build();
+        assertTrue(user.isFriendWith(SOONHEE));
+    }
 }
