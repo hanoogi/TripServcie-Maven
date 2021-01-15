6de0aa0dd500fa38877e80efd1c5a2b9f4bf57d3
Hyejin-Lee-ANC <66674193+Hyejin-Lee-ANC@users.noreply.github.com>
2021-01-15 11:58:26 +0900

    친구가 아닌 경우 trip을 반환하지 않는 경우 테스트

--- src/test/java/com/bestreviewer/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/com/bestreviewer/tripservicekata/trip/TripServiceTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class TripServiceTest {
     private static final User NOT_LOGGED_USER = null;
     private static final User LOGGED_USER = new User();
+    private static final User ANOTHER_USER = new User();
+    private static final Trip TO_JEJU = new Trip();
 
     private User loggedInUser;
+
 
     @Test
     void test_fail() {
@@ -23,6 +29,19 @@ public class TripServiceTest {
     public void testThrowExceptionWhenNotLogged(){
         TripService tripService = new TestableTripService();
         Assertions.assertThrows(Exception.class,()-> tripService.getTripsByUser(NOT_LOGGED_USER));
+    }
+
+    @Test
+    @DisplayName("친구가 아닌 경우 trip 을 반환하지 않는다.")
+    public void testReturnNoTripWhenUserNotFriend(){
+        TripService tripService = new TestableTripService();
+        loggedInUser = LOGGED_USER;
+        User friend = new User();
+        friend.addFriend(ANOTHER_USER);
+        friend.addTrip(TO_JEJU);
+
+        List<Trip> friendTrips = tripService.getTripsByUser(friend);
+        assertEquals(0,friendTrips.size());
     }
 
     private class TestableTripService extends TripService{
