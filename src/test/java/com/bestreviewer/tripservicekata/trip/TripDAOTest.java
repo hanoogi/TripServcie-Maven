d083024ec3f5aa879d86dc204374178f0a7df2f0
Hyejin-Lee-ANC <66674193+Hyejin-Lee-ANC@users.noreply.github.com>
2021-01-15 13:58:35 +0900

    TripDAOTest 작성 : TripDAO 를 mocking 하기 전 테스트 작성

--- src/test/java/com/bestreviewer/tripservicekata/trip/TripDAOTest.java
+++ src/test/java/com/bestreviewer/tripservicekata/trip/TripDAOTest.java
@@ -1,5 +1,13 @@
 package com.bestreviewer.tripservicekata.trip;
 
+import com.bestreviewer.tripservicekata.user.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TripDAOTest {
+    @Test
+    public void testThrowExceptionWhenRetrieveUserTrips(){
+        Assertions.assertThrows(Exception.class,()-> TripDAO.findTripsByUser(new User()));
+    }
 
 }
