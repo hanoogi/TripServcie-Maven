dce4575df0dfaacb2128ee22b68ee5a79fa49da9
Hyejin-Lee-ANC <66674193+Hyejin-Lee-ANC@users.noreply.github.com>
2021-01-15 13:31:07 +0900

    getTripsByUser() 의 바깥 if() 수정

--- src/main/java/com/bestreviewer/tripservicekata/trip/TripService.java
+++ src/main/java/com/bestreviewer/tripservicekata/trip/TripService.java
@@ -12,20 +12,17 @@ public class TripService {
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedUser = getLoggedInUser();
-		if (loggedUser != null) {
+		if (loggedUser == null) {
+			throw new UserNotLoggedInException();
+
+		} else {
 			boolean isFriend = user.isFriendWith(loggedUser);
-//			for (User friend : user.getFriends()) {
-//				if (friend.equals(loggedUser)) {
-//					isFriend = true;
-//					break;
-//				}
-//			}
+
 			if (isFriend) {
 				tripList = tripsBy(user);
 			}
 			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
+
 		}
 	}
 
