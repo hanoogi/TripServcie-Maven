75ac1f6f740f0bd36f1a539fb1b1447fffd2fe24
Hyejin-Lee-ANC <66674193+Hyejin-Lee-ANC@users.noreply.github.com>
2021-01-17 12:38:20 +0900

    친구가 아닌 경우 테스트 추가 : Fail - User type 별 상수로 정의 - 친구가 아닌 경우 trip 을 반환하지 않는 테스트 추가

--- src/test/java/com/bestreviewer/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/com/bestreviewer/tripservicekata/trip/TripServiceTest.java
@@ -4,10 +4,18 @@ import com.bestreviewer.tripservicekata.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TripServiceTest {
+
+    private static final User NOT_LOGGED_USER = null;
+    private static final User LOGGED_USER = null;
+    private static final User ANOTHER_USER = new User();
+    private static final Trip TO_JEJU = new Trip();
+
+    private User loggedUser;
     @Test
     void test_fail() {
         //fail();
@@ -17,14 +25,29 @@ public class TripServiceTest {
     @DisplayName("Should throw on exception when user is not logged.")
     public void testThrowExceptionWhenNotLogged(){
         TripService tripService = new TestableTripService();
+        loggedUser = NOT_LOGGED_USER;
 
-        assertThrows(Exception.class,()->tripService.getTripsByUser(null));
+        assertThrows(Exception.class,()->tripService.getTripsByUser(loggedUser));
+    }
+
+    @Test
+    @DisplayName("친구가 아닌 경우 trip 을 반환하지 않는다.")
+    public void testReturnNoTripWhenUserNotFriend(){
+        TripService tripService = new TestableTripService();
+
+        loggedUser = LOGGED_USER;
+        User friend = new User();
+        friend.addFriend(ANOTHER_USER);
+        friend.addTrip(TO_JEJU);
+
+        List<Trip> friendTrips = tripService.getTripsByUser(friend);
+        assertEquals(0,friendTrips.size());
     }
 
     private class TestableTripService extends TripService{
         @Override
         protected User getLoggedUser(){
-            return null;
+            return loggedUser;
         }
     }
 }
