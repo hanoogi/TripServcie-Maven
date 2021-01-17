8f40f026aff788afa42a1938f41b84d4d729a888
Hyejin-Lee-ANC <66674193+Hyejin-Lee-ANC@users.noreply.github.com>
2021-01-18 00:05:39 +0900

    Refactoring  - getTripsByUser() 의 loggedUser 변수대신 getLoggedUser() 메소드 호출로 변경

--- src/main/java/com/bestreviewer/tripservicekata/trip/TripService.java
+++ src/main/java/com/bestreviewer/tripservicekata/trip/TripService.java
@@ -12,8 +12,7 @@ public class TripService {
 	public static final ArrayList<Trip> NO_TRIPS = new ArrayList<Trip>();
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		User loggedUser = getLoggedUser();
-		return getTripsBy(user, loggedUser);
+		return getTripsBy(user, getLoggedUser());
 	}
 
 	private List<Trip> getTripsBy(User user, User loggedUser) {
