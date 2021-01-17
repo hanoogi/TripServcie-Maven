14003505c1a7f12dc0eb98ad37ed7e7128568361
Hyejin-Lee-ANC <66674193+Hyejin-Lee-ANC@users.noreply.github.com>
2021-01-17 12:16:10 +0900

    TripService부터 상속받은 TestableTripService 테스트 대상 변경

--- src/test/java/com/bestreviewer/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/com/bestreviewer/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,6 @@
 package com.bestreviewer.tripservicekata.trip;
 
+import com.bestreviewer.tripservicekata.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,8 +16,15 @@ public class TripServiceTest {
     @Test
     @DisplayName("Should throw on exception when user is not logged.")
     public void testThrowExceptionWhenNotLogged(){
-        TripService tripService = new TripService();
+        TripService tripService = new TestableTripService();
 
         assertThrows(Exception.class,()->tripService.getTripsByUser(null));
+    }
+
+    private class TestableTripService extends TripService{
+        @Override
+        protected User getLoggedUser(){
+            return null;
+        }
     }
 }
