788ecdfd8cf63fbf4478e7ac1bee25ccd0fb3f15
Hyejin-Lee-ANC <66674193+Hyejin-Lee-ANC@users.noreply.github.com>
2021-01-17 14:22:24 +0900

    친구인 경우 trip 반환 테스트 추가 - 친구인 경우 친구의 trips 반환하는 경우 테스트 - 테스트 코드 refactoring

--- src/test/java/com/bestreviewer/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/com/bestreviewer/tripservicekata/trip/TripServiceTest.java
@@ -2,6 +2,7 @@ package com.bestreviewer.tripservicekata.trip;
 
 import com.bestreviewer.tripservicekata.exception.UserNotLoggedInException;
 import com.bestreviewer.tripservicekata.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,9 +16,16 @@ public class TripServiceTest {
     private static final User LOGGED_USER = new User();
     private static final User ANOTHER_USER = new User();
     private static final Trip TO_JEJU = new Trip();
+    private static final Trip TO_BUSAN = new Trip();
 
     private User loggedUser;
     TripService tripService;
+
+    @BeforeEach
+    public void setup(){
+        tripService = new TestableTripService();
+        loggedUser = LOGGED_USER;
+    }
 
     @Test
     void test_fail() {
@@ -27,24 +35,31 @@ public class TripServiceTest {
     @Test
     @DisplayName("Should throw on exception when user is not logged.")
     public void throwsExceptionWhenNotLoggedIn(){
-        tripService = new TestableTripService();
-        loggedUser = NOT_LOGGED_USER;
-
-        assertThrows(Exception.class,()->tripService.getTripsByUser(loggedUser));
+        assertThrows(Exception.class,()->tripService.getTripsByUser(NOT_LOGGED_USER));
     }
 
     @Test
     @DisplayName("친구가 아닌 경우 trip 을 반환하지 않는다.")
     public void returnsNoTripForUserWithNoFriend(){
-        tripService = new TestableTripService();
-
-        loggedUser = LOGGED_USER;
         User friend = new User();
         friend.addFriend(ANOTHER_USER);
         friend.addTrip(TO_JEJU);
 
         List<Trip> friendTrips = tripService.getTripsByUser(friend);
         assertTrue(friendTrips.isEmpty());
+    }
+
+    @Test
+    @DisplayName("친구인 경우 trip 을 반환한다.")
+    public void testReturnTripWhenUserAreFriend(){
+        User friend = new User();
+        friend.addFriend(ANOTHER_USER);
+        friend.addFriend(loggedUser);
+        friend.addTrip(TO_JEJU);
+        friend.addTrip(TO_BUSAN);
+
+        List<Trip> friendTrips = tripService.getTripsByUser(friend);
+        assertTrue(friendTrips.contains(TO_JEJU) && friendTrips.contains(TO_BUSAN));
     }
 
     private class TestableTripService extends TripService{
